3cc68f5aa489d223f34270f02cadf06ec6f55456
José Luis Sánchez-Porro Godoy <jose.sanchezporro@mirada.tv>
2018-10-28 22:23:25 +0100

    Initial Commit

---
+++ TransactionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TransactionApp
+//
+//  Created by jose.sanchezporro on 28/10/2018.
+//  Copyright © 2018 jose.sanchezporro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
